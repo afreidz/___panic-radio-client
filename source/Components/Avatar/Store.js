458d1b3913db485fd18b782d321ea1a77333b073
afreidz <afreidenfelds@gmail.com>
2020-01-12 12:27:07 -0600

    update path to vote mp3s

--- source/Components/Avatar/Store.js
+++ source/Components/Avatar/Store.js
@@ -5,8 +5,8 @@ export const votes = derived([socket], ([$socket], set) => {
   let state = [];
   $socket.onhostmessage('vote', data => {
     state = data.votes;
-    if (data.sound === 'like') new Audio('Assets/like.mp3').play();
-    if (data.sound === 'dislike') new Audio('Assets/dislike.mp3').play();
+    if (data.sound === 'like') new Audio('./assets/like.mp3').play();
+    if (data.sound === 'dislike') new Audio('./assets/dislike.mp3').play();
     set(state);
   });
   $socket.onhostmessage('clear-votes', () => {
